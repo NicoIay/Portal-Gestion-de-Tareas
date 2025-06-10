3607ee4c3120f83dda713b928419eac54c5963dc
Nicolay <124218595+NicoIay@users.noreply.github.com>
2025-06-10 06:58:53 +0000

    cambio de diseño en index y cambios de dia y noche

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ function volverPagina() {
       window.location.href = "index.html"; // o cualquier página anterior
     }
   }
+
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("toggle");
+  
+    // Aplicar modo guardado
+    const modoGuardado = localStorage.getItem("modo");
+    if (modoGuardado === "oscuro") {
+      document.body.classList.add("modo-oscuro");
+      toggle.checked = true;
+    }
+  
+    // Escuchar cambio del checkbox
+    toggle.addEventListener("change", () => {
+      document.body.classList.toggle("modo-oscuro");
+  
+      const modoActual = document.body.classList.contains("modo-oscuro")
+        ? "oscuro"
+        : "claro";
+      localStorage.setItem("modo", modoActual);
+    });
+  });
